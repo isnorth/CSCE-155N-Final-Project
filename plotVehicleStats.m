435cd59978f4de6e79c3ddeb6447c74ee80f4387
isnorth <isnorth@users.noreply.github.com>
2020-08-14 10:03:13 -0500

    Update plotVehicleStats.m
    Alter plotVehicleStats.m to take in the upper and lower bounds as a two-element matrix. Rearrange inputs so as to be organized by plot.

--- plotVehicleStats.m
+++ plotVehicleStats.m
@@ -1,14 +1,12 @@
-function [] = plotVehicleStats(powerLowerBound,powerUpperBound, ... 
-    fuelLowerBound,fuelUpperBound, ...
-    C_0,C_1,C_2,T_E,E_g,P)
+function [] = plotVehicleStats(powerBounds,C_0,C_1,C_2,fuelBounds,T_E,E_g,P)
     %This function produces two graphs, the first shows the effect
     %of input power on efficiency, the second shows the effect of 
     %fuel capacity on endurance. This function was created to fulfill
     %the requirements of UNL's CSCE 155N Summer 2020's final project. 
     %To produce the graphs used in the final project:
-        %powerLowerBound = 0; powerUpperBound = 125; 
-        %fuelLowerBound = 25; fuelUpperBound = 100;
+        %powerBounds = [0,125]; %input power (kW)
         %C_0 = 0.6; C_1 = 0.02; C_2 = 0.0015;
+        %fuelBounds = [25,100]; %fuel capacity (l)
         %T_E = 0.25; %thermal efficiency
         %E_g = 8.8; %gasoline energy (kWh/l)
         %P = 298; %engine power (kW)
@@ -20,7 +18,7 @@ function [] = plotVehicleStats(powerLowerBound,powerUpperBound, ...
     subplot(2,1,1); 
     
     %set range of values for P_in
-    P_in = powerLowerBound:0.1:powerUpperBound;
+    P_in = powerBounds(1):0.1:powerBounds(2);
     
     %calculate power loss
     P_loss = C_0 + C_1.*P_in + C_2.*(P_in.^2);
@@ -35,14 +33,14 @@ function [] = plotVehicleStats(powerLowerBound,powerUpperBound, ...
     title('The Effect of Power In on Efficiency'); %title plot same as figure
     xlabel('Power In (kW)'); %label x-axis
     ylabel('Efficiency'); %label y-axis
-    axis([powerLowerBound,powerUpperBound,0,1]); %ensure entire graph fits on graph
+    axis([powerBounds(1),powerBounds(2),0,1]); %ensure entire graph fits on graph
     
     
     %Equations for SECOND plot
     subplot(2,1,2);
     
     %set range for fuel capacity (l) as a matrix
-    C_f = fuelLowerBound:0.1:fuelUpperBound; 
+    C_f = fuelBounds(1):0.1:fuelBounds(2); 
     
     %calculate fuel flow (R_f) as a constant
     R_f = P/(E_g*T_E);
@@ -55,6 +53,6 @@ function [] = plotVehicleStats(powerLowerBound,powerUpperBound, ...
     title('The Effect of Fuel Capacity on Endurance'); %title plot same as figure
     xlabel('Fuel Capacity (l)'); %label x-axis
     ylabel('Endurance'); %label y-axis
-    axis([fuelLowerBound,fuelUpperBound,0,1]); %ensure entire graph fits on graph
+    axis([fuelBounds(1),fuelBounds(2),0,1]); %ensure entire graph fits on graph
     
 end
